29c4df49ef23cd90fabb5164d4763696b5fa853a
andrewyang17 <50484992+andrewyang17@users.noreply.github.com>
2022-10-20 23:16:23 +0800

    add comments

--- foundation/blockchain/database/account.go
+++ foundation/blockchain/database/account.go
@@ -64,6 +64,7 @@ func has0xPrefix(a AccountID) bool {
 	return len(a) >= 2 && a[0] == '0' && (a[1] == 'x' || a[1] == 'X')
 }
 
+// isHex validates whether each byte is valid hexadecimal string.
 func isHex(a AccountID) bool {
 	if len(a)%2 != 0 {
 		return false
@@ -78,6 +79,7 @@ func isHex(a AccountID) bool {
 	return true
 }
 
+// isHexCharacter returns bool of c being a valid hexadecimal.
 func isHexCharacter(c byte) bool {
 	return ('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')
 }
